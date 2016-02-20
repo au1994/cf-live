2220555b826b5c374c2de23051e715598080078d
au1994 <abhishek.upadhyay.cse12@iitbhu.ac.in>
2016-02-20 18:40:30 +0530

    mail service added
    now can mail

--- cf-resources/src/main/java/cf/UserController.java
+++ cf-resources/src/main/java/cf/UserController.java
@@ -1,13 +1,22 @@
 package cf;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import com.mongodb.*;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
+
+import cf.CfService;
+import cf.User;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 /**
  * Created by abhishekupadhyay on 2016/02/16.
@@ -16,12 +25,51 @@ import java.util.UUID;
 @RequestMapping(value="/urr")
 class UserController {
 
+    private final CfService cfService;
+
+
+    @Autowired
+    UserController(CfService cfService)
+    {
+        this.cfService=cfService;
+    }
+
     @RequestMapping(value="/ur")
-    public Map<String,Object> home() {
-        Map<String,Object> model = new HashMap<String,Object>();
-        model.put("id", UUID.randomUUID().toString());
-        model.put("content", "Hello World");
-        return model;
+    public List<DBObject> home() {
+
+
+
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.register(MailSenderConfig.class);
+        ctx.refresh();
+        JavaMailSenderImpl mailSender = ctx.getBean(JavaMailSenderImpl.class);
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper mailMsg = new MimeMessageHelper(mimeMessage);
+        try {
+            mailMsg.setFrom("abhishek@craftemporio.com");
+            mailMsg.setTo("abhishek.upadhyay.cse12@iitbhu.ac.in");
+            mailMsg.setSubject("Test mail From CraftEmporio");
+            mailMsg.setText("Hello World!");
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
+        mailSender.send(mimeMessage);
+        System.out.println("---Done---");
+
+
+
+
+        DBObject dbobj = new BasicDBObject();
+        List<DBObject> dblist= null;
+        try {
+            dblist = cfService.find(dbobj);
+            System.out.println(dblist);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return dblist;
     }
     }
 
